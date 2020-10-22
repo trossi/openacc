4888d5b1706987080202e81548855bb745023cc5
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2020-10-22 11:32:19 +0300

    Add more output to sbatch script

--- exercises/hello-world/job.sh
+++ exercises/hello-world/job.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
-#SBATCH --account=FIXME
+#SBATCH --account=project_2003514
 #SBATCH --ntasks=1
 #SBATCH --time=00:15:00
 #SBATCH --gres=gpu:v100:1
-#SBATCH --partition=gputest
+##SBATCH --partition=gputest
+#SBATCH --partition=gpu
+#SBATCH --reservation=gpuhackathon
 
-module load pgi/19.7 cuda/10.1.168
 
-srun ./hello
+module load pgi/19.7
+# module load pgi/19.7 cuda/10.1.168
+
+echo "---- info"
+pgaccelinfo -v
+echo -e "---- end\n"
+
+echo "---- hello c"
+srun ./hello-c.exe
+echo -e "---- end\n"
+
+echo "---- hello c acc"
+srun ./hello-c-acc.exe
+echo -e "---- end\n"
+
+echo "---- hello f"
+srun ./hello-f.exe
+echo -e "---- end\n"
+
+echo "---- hello f acc"
+srun ./hello-f-acc.exe
+echo -e "---- end\n"
+
 
 # Submit to the batch job queue with the command:
 #  sbatch job.sh
-
-# or alternatively, run directly from the command line:
-#  srun --account=FIXME --ntasks=1 --time=00:15:00 --gres=gpu:v100:1 \
-#       --partition=gputest ./hello
